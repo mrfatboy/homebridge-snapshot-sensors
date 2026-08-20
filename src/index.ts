b4999e6a9b6bacccda38069d548a7a61ef97117a
mrfatboy <mrfatboy@sbcglobal.net>
2026-08-20 11:47:22 -0700

    Rename platform class and registration

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import { API } from 'homebridge';
-import { StreamSensorsPlatform } from './platform.js';
+import { SnapshotSensorsPlatform } from './platform.js';
 import { PLATFORM_NAME, PLUGIN_NAME } from './settings.js';
 
 export default (api: API) => {
-  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, StreamSensorsPlatform);
+  api.registerPlatform(PLUGIN_NAME, PLATFORM_NAME, SnapshotSensorsPlatform);
 };
