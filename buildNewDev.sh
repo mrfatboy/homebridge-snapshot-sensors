8980f97c19034b90728964f4285a26a9a01a769a
mrfatboy <mrfatboy@sbcglobal.net>
2026-08-25 15:07:38 -0700

    Restore README and update development build script

--- buildNewDev.sh
+++ buildNewDev.sh
@@ -3,7 +3,7 @@
 clear
 
 cd ~/HomebridgeDev/homebridge-snapshot-sensors
-git pull origin NewCore
+git pull origin NewDev
 npm run build
 npm pack
 sudo npm install --prefix /var/lib/homebridge ./homebridge-snapshot-sensors-1.0.0.tgz
