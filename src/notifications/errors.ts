23d034ef62508fff1c7f6bc8593556d1025db24c
mrfatboy <mrfatboy@sbcglobal.net>
2026-09-03 03:18:27 -0700

    Remove unused notification error type

--- src/notifications/errors.ts
+++
@@ -1,6 +0,0 @@
-export class NotificationError extends Error {
-  constructor(message: string, public readonly configuration = false) {
-    super(message);
-    this.name = 'NotificationError';
-  }
-}
